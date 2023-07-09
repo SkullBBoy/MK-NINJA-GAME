c15d7f22f44799568581b8141ac0a6da6faf2e07
Oliva <roman57izqui@hotmail.com>
2023-07-09 06:34:51 -0300

    Puntuacion y Racha Agregados.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const respuestafoto = document.getElementById("respuestafoto");
     const puntuacionElement = document.getElementById("puntuacion");
     const rachaElement = document.getElementById("racha");
+    const btnRespuesta = document.getElementById("btnRespuesta")
     const ninjas = [
       { nombre: "scorpion" },
       { nombre: "subzero" },
@@ -24,41 +25,46 @@ document.addEventListener("DOMContentLoaded", function() {
   
     mostrarNinjaActual();
   
-    document.getElementById("btnRespuesta").addEventListener("click", function() {
-      const valorInput = input.value.toLowerCase();
-  
-      if (valorInput === "") {
-        alert("Por favor, ingresa una respuesta.");
-        return;
-      }
-  
-      if (valorInput === ninjas[ninjaActual].nombre) {
-        respuestafoto.src = "./assets/bien.png";
-        input.value = "";
-        puntuacion += 10;
-        racha += 1;
-        setTimeout(function() {
-          ninjaActual = (ninjaActual + 1) % ninjas.length;
-          localStorage.setItem("NinjaActual", ninjaActual);
-          localStorage.setItem("Puntuacion", puntuacion);
-          localStorage.setItem("Racha", racha);
-          mostrarNinjaActual();
-        }, 2500);
-      } else {
-        respuestafoto.src = "./assets/mal.png";
-        input.value = "";
-        puntuacion -= 10;
-        if (puntuacion < 0) {
-          puntuacion = 0;
-        }
-        racha = 0;
-        setTimeout(function() {
-          respuestafoto.src = "./assets/pregunta.png";
-          localStorage.setItem("Puntuacion", puntuacion);
-          localStorage.setItem("Racha", racha);
-          mostrarNinjaActual();
-        }, 2500);
-      }
-    });
+    if(btnRespuesta){
+        btnRespuesta.addEventListener("click", function() {
+            const valorInput = input.value.toLowerCase();
+        
+            if (valorInput === "") {
+              alert("Por favor, ingresa una respuesta.");
+              return;
+            }
+        
+            if (valorInput === ninjas[ninjaActual].nombre) {
+              respuestafoto.src = "./assets/bien.png";
+              input.value = "";
+              puntuacion += 10;
+              racha += 1;
+              setTimeout(function() {
+                ninjaActual = (ninjaActual + 1) % ninjas.length;
+                localStorage.setItem("NinjaActual", ninjaActual);
+                localStorage.setItem("Puntuacion", puntuacion);
+                localStorage.setItem("Racha", racha);
+                mostrarNinjaActual();
+              }, 2500);
+            } else {
+              respuestafoto.src = "./assets/mal.png";
+              input.value = "";
+              puntuacion -= 10;
+              if (puntuacion < 0) {
+                puntuacion = 0;
+              }
+              racha = 0;
+              setTimeout(function() {
+                respuestafoto.src = "./assets/pregunta.png";
+                localStorage.setItem("Puntuacion", puntuacion);
+                localStorage.setItem("Racha", racha);
+                mostrarNinjaActual();
+              }, 2500);
+            }
+          });
+    }
+    
+    
+    
   });
   
